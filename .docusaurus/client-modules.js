5ab442ebf428e68f18574172d8779a8334ab29f9
carleneoestergaard <carlene@inviso.dk>
2021-06-15 14:16:41 +0200

    c

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/carlenejones/GitHub/carleneoestergaard.github.io/src/css/custom.css"),
+];
---
+++ .docusaurus/docusaurus.config.js
@@ -0,0 +1,135 @@
+export default {
+  "title": "Inviso Alteryx Documetation",
+  "tagline": "Alteryx rules the world!",
+  "url": "https://carleneoestergaard.github.io",
+  "baseUrl": "/",
+  "onBrokenLinks": "throw",
+  "onBrokenMarkdownLinks": "warn",
+  "favicon": "img/favicon.ico",
+  "organizationName": "carleneoestergaard",
+  "projectName": "carleneoestergaard.github.io",
+  "themeConfig": {
+    "navbar": {
+      "title": "Alteryx Documentation",
+      "logo": {
+        "alt": "My Site Logo",
+        "src": "img/Inviso_Logo.png"
+      },
+      "items": [
+        {
+          "type": "doc",
+          "docId": "intro",
+          "position": "left",
+          "label": "Designer",
+          "activeSidebarClassName": "navbar__link--active"
+        },
+        {
+          "to": "/blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "href": "https://github.com/carleneoestergaard/inviso-alteryx",
+          "label": "GitHub",
+          "position": "right"
+        }
+      ],
+      "hideOnScroll": false
+    },
+    "footer": {
+      "style": "dark",
+      "links": [
+        {
+          "title": "Docs",
+          "items": [
+            {
+              "label": "Designer",
+              "to": "/docs/intro"
+            }
+          ]
+        },
+        {
+          "title": "Community",
+          "items": [
+            {
+              "label": "Stack Overflow",
+              "href": "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              "label": "Discord",
+              "href": "https://discordapp.com/invite/docusaurus"
+            },
+            {
+              "label": "Twitter",
+              "href": "https://twitter.com/docusaurus"
+            }
+          ]
+        },
+        {
+          "title": "More",
+          "items": [
+            {
+              "label": "Blog",
+              "to": "/blog"
+            },
+            {
+              "label": "GitHub",
+              "href": "https://github.com/facebook/docusaurus"
+            }
+          ]
+        }
+      ]
+    },
+    "colorMode": {
+      "defaultMode": "light",
+      "disableSwitch": false,
+      "respectPrefersColorScheme": false,
+      "switchConfig": {
+        "darkIcon": "🌜",
+        "darkIconStyle": {},
+        "lightIcon": "🌞",
+        "lightIconStyle": {}
+      }
+    },
+    "docs": {
+      "versionPersistence": "localStorage"
+    },
+    "metadatas": [],
+    "prism": {
+      "additionalLanguages": []
+    },
+    "hideableSidebar": false
+  },
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "sidebarPath": "/Users/carlenejones/GitHub/carleneoestergaard.github.io/sidebars.js",
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/"
+        },
+        "blog": {
+          "showReadingTime": true,
+          "editUrl": "https://github.com/facebook/docusaurus/edit/master/website/blog/"
+        },
+        "theme": {
+          "customCss": "/Users/carlenejones/GitHub/carleneoestergaard.github.io/src/css/custom.css"
+        }
+      }
+    ]
+  ],
+  "baseUrlIssueBanner": true,
+  "i18n": {
+    "defaultLocale": "en",
+    "locales": [
+      "en"
+    ],
+    "localeConfigs": {}
+  },
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": [],
+  "titleDelimiter": "|",
+  "noIndex": false
+};
